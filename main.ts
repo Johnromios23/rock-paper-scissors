60b75db70de5e554108b35ab7d04c5f34a78f0e7
Johnromios23 <114350014+Johnromios23@users.noreply.github.com>
2022-10-19 23:38:38 +0300

    Αρχικά αρχεία για έργο MakeCode

---
+++ main.ts
@@ -0,0 +1,35 @@
+let hand = 0
+input.onGesture(Gesture.Shake, function () {
+    hand = randint(0, 2)
+    if (hand == 0) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (hand == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    game.addScore(1)
+    basic.pause(100)
+    basic.showString("Wins:")
+    basic.showNumber(game.score())
+})
